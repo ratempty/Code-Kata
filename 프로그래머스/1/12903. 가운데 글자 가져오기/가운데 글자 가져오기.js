e165581dfc3e742ba2218781e66a1b6fe91887c7
ratempty <138560050+ratempty@users.noreply.github.com>
2024-01-05 11:01:19 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -4,12 +4,9 @@ function solution(s) {
     let midNum = s.length/2;
     let oddEven = Math.floor(s.length/2) ;
     if(s.length % 2 === 0){
-            //짝수
         answer=arr[midNum-1] +arr[midNum]
     }else{
-           //홀수
         answer=arr[oddEven]  
     }
-    
     return answer;
 }
