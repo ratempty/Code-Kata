7d29eb4645ee035682edf197534f7d4dcdd8a3a3
ratempty <138560050+ratempty@users.noreply.github.com>
2024-02-15 21:28:49 +0900

    [level 2] Title: 피보나치 수, Time: 8.50 ms, Memory: 39 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -3,8 +3,21 @@ function solution(n) {
     let arr  = [0,1];
     for(let i = 2 ; i <= n ; i++){
         let curr = arr[i-1] + arr[i-2];
-        arr.push(curr% 1234567)
+        arr.push(curr % 1234567)
     }
     answer = arr[n] 
+    console.log(arr)
     return answer;
 }
+
+// function solution(n) {
+//     var answer = 0;
+//     let arr  = [0,1];
+//     for(let i = 2 ; i <= n ; i++){
+//         let curr = arr[i-1] + arr[i-2];
+//         arr.push(curr)
+//     }
+//     answer = arr[n] % 1234567
+//     console.log(arr)
+//     return answer;
+// }
